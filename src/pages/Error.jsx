1d29d4f115d48098a12224526821e714710520d7
guilhemkzk <guilhem.l.krawczyk@gmail.com>
2023-05-23 15:17:38 +0200

    Creating error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import error from "../assets/images/404.webp";
+
 function Error() {
-  return <div>404</div>;
+  return (
+    <div className="error">
+      <img src={error} alt="Erreur" />
+
+      <span>Oups! La page que vous demandez n'existe pas.</span>
+      <p>
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Error;
